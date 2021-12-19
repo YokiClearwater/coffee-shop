2284cb08a0c0dce3f7abd7c1819f19bf8fb721d9
Yoki Clearwater <87857995+YokiClearwater@users.noreply.github.com>
2021-12-19 23:16:53 +0700

    Read and Write Data from/to Linked List

--- CRUD.cpp
+++ CRUD.cpp
@@ -4,18 +4,17 @@ using namespace std;
 
 struct Coffee {
     string id, name;
-    float price;
+    string price;
     Coffee *next;
     Coffee *prev;
 };
-
 struct List {
     int n;
     Coffee *head;
     Coffee *tail;
 };
 
-List *createList() {
+List *createEmptyList() {
     List *ls = new List();
 
     ls->n = 0;
@@ -25,7 +24,7 @@ List *createList() {
     return ls;
 }
 
-void insertBegin(List *ls, string itemID, string itemName, float itemPrice) {
+void insertBegin(List *ls, string itemID, string itemName, string itemPrice) {
     Coffee *item = new Coffee();
     item->id = itemID;
     item->name = itemName;  
@@ -44,9 +43,72 @@ void insertBegin(List *ls, string itemID, string itemName, float itemPrice) {
     ls->n += 1;
 }
 
+void displayList(List *ls) {
+    Coffee *temp = new Coffee();
+    temp = ls->head;
+    while(temp != NULL) {
+        cout << temp->id << "\t" << temp->name << "\t" << temp->price << endl;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void displayReverseList(List *ls) {
+    Coffee *temp = new Coffee();
+    temp = ls->tail;
+    while(temp != NULL) {
+        cout << temp->id << "\t" << temp->name << "\t" << temp->price << endl;
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+void writeFile(List *ls) {
+    ofstream file;
+    file.open("ItemList.txt");
+
+    Coffee *temp = new Coffee();
+    temp = ls->head;
+    while(temp != NULL) {
+        file << temp->id << "\t" << temp->name << "\t" << temp->price << endl;
+        temp = temp->next;
+    }
+
+    file.close();
+}
+
+List *readFile() {
+    ifstream file;
+    file.open("ItemList.txt");
+    string id, name, price;
+
+    List *L2 = createEmptyList();
+
+    while(file >> id >> name >> price) {
+        insertBegin(L2, id, name, price);
+    }
+
+    file.close();
+    
+    return L2; 
+}
 
 int main() {
+    List *L1 = createEmptyList();
 
+    insertBegin(L1, "1", "Latte", "123.5");
+    insertBegin(L1, "2", "Americano", "123.5");
+    insertBegin(L1, "3", "Ice-Cream", "123.5");
+
+    displayList(L1);
+
+    displayReverseList(L1);
+
+    writeFile(L1);
+
+    List *L2 = readFile(); // Text File Shouldn't have a space
+
+    displayList(L2);
 
     return 0;
 }
